8b832f5b67d50fd438d5d4712e614f7e219fe306
PhilCAEUser <rbyETE4Ceu2iA4Z>
2021-02-13 08:43:49 +0000

    Init

---
+++ db/7008_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 7008, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tests.
+--
+CREATE TABLE caeschema.tests (
+  sd INTEGER ,
+CONSTRAINT sd_PK PRIMARY KEY (sd)
+ 
+);
+
+
+
